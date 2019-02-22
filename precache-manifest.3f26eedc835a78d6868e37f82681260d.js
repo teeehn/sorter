521483e3cddef45df260ee30609347f4129dfff1
Tom Nicolosi <tom@teeehn.com>
2019-02-22 00:13:51 -0800

    Updates

---
+++ precache-manifest.3f26eedc835a78d6868e37f82681260d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "c974619f13285b2c8b22",
+    "url": "/my-app/static/js/main.c974619f.chunk.js"
+  },
+  {
+    "revision": "6d122b40f4cd84ba9bd6",
+    "url": "/my-app/static/js/2.6d122b40.chunk.js"
+  },
+  {
+    "revision": "c974619f13285b2c8b22",
+    "url": "/my-app/static/css/main.4ef5d3bf.chunk.css"
+  },
+  {
+    "revision": "3729229ad343bfb9a70b850f893110fb",
+    "url": "/my-app/index.html"
+  }
+];
